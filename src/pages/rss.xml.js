73ba83480a760cca7a0a81a364a9af3358cc3db0
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2024-03-07 23:53:11 +0000

    Updated rss feed to include posts from all collections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,36 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../consts';
+
+// A helper function to standardize the structure of feed items
+function createFeedItem(post, section) {
+  return {
+    title: post.data.title,
+    description: post.data.description,
+    link: `${SITE_URL}/${section}/${post.slug}/`,
+    guid: `${SITE_URL}/${section}/${post.slug}/`,
+    pubDate: new Date(post.data.published_at).toUTCString(),
+    author: post.data.author,
+    categories: post.data.tags ? post.data.tags.split(',') : [], // Check if tags exist and split
+  };
+}
 
 export async function GET(context) {
-  	const posts = await getCollection('blog');
-  	return rss({
-    	title: SITE_TITLE,
-    	description: SITE_DESCRIPTION,
-    	site: context.site,
-    	items: posts.map((post) => ({
-      		...post.data,
-      		link: `/blog/${post.slug}/`,
-      		pubDate: new Date(post.data.published_at).toUTCString(), // Convert the date string to a valid date format
-    	})).filter(item => item.data.status !== 'draft'), // Filter out draft posts
-  	});
+  // Fetch posts from all collections
+  const blogPosts = (await getCollection('blog')).map(post => createFeedItem(post, 'blog'));
+  const reviewPosts = (await getCollection('reviews')).map(post => createFeedItem(post, 'reviews'));
+  const travelPosts = (await getCollection('travel')).map(post => createFeedItem(post, 'travel'));
+  const articlePosts = (await getCollection('articles')).map(post => createFeedItem(post, 'articles'));
+
+  // Combine all posts into a single array and sort them by publication date
+  const allPosts = [...blogPosts, ...reviewPosts, ...travelPosts, ...articlePosts]
+    .filter(item => item.data.status !== 'draft') // Filter out draft posts
+    .sort((a, b) => new Date(b.data.published_at) - new Date(a.data.published_at)); // Sort by date, latest first
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: SITE_URL,
+    items: allPosts
+  });
 }
