b99980222e70489667072052432f2927fd671aba
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2026-04-28 13:43:51 +1000

    added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   output: 'static',
   site: 'https://example.com',
+  adapter: cloudflare(),
   base: '/',
   integrations: [mdx(), sitemap()],
 });
