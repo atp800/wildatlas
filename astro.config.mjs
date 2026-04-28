108e17b4d380e0d76cf3c96a01cd9fb288627136
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2026-04-28 13:34:07 +1000

    edited astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://example.com',
   base: '/',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
 });
