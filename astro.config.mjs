43671181b46badbc5e37b447884aae88febaa858
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2026-04-28 13:49:10 +1000

    imported cloudflare installation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://example.com',
+  site: 'https://wildatlas.org',
   adapter: cloudflare(),
   base: '/',
   integrations: [mdx(), sitemap()],
